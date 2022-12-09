156219ec671f7cb224681e246418d0f0ba44ea91
iwanbillion <119886422+iwanbillion@users.noreply.github.com>
2022-12-09 11:27:52 +0100

    PIERWSZY UPDATE ZAGRAJ TERAZ

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import randint from random
+from random import randint
 import time
 
 print("wylosuj 6 aby zacząć gre")
-time.sleep(4)
+time.sleep(2)
 x = randint(1,100)
 print(x)
+if x != 6:
+    print("dupa się zesrała")
+else:
+    print("WRZECIONO THE GAME")
+
