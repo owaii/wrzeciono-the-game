7c6c813e78257ec3e7514f878c7ca20bf752b377
iwanbillion <119886422+iwanbillion@users.noreply.github.com>
2022-12-16 11:13:30 +0100

    Add files via upload

---
+++ wrzecionothegame.py
@@ -0,0 +1,26 @@
+from random import randint
+import time
+#---------------------------------
+x = 0
+z = 0
+#---------------------------------
+def move(x,z):
+    while True:
+        print("KOORDYNATY: ", 'x=', x, "z=", z)
+        print("a) Północ")
+        print("b) Południe")
+        print("c) Zachód")
+        print("d) Wschód")
+        respond = input('')
+        if respond == "a" or "A":
+            x += 10
+        if respond == "b" or "B":
+            x -= 10
+        if respond == "c" or "C":
+            z -= 10
+        if respond == "d" or "D":
+            z += 10
+#---------------------------------
+print("WRZECIONO THE GAME")
+time.sleep(2)
+move(x,z)
